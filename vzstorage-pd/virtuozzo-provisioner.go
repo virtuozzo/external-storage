d624aa75120c2867a0479b2f08152e953395777a
Anton Kurbatov <akurbatov@virtuozzo.com>
2017-08-24 21:31:26 +0300

    Remove deltasPath option
    Do not complicate logic and store image always with volumes data
    
    Signed-off-by: Anton Kurbatov <akurbatov@virtuozzo.com>

--- vzstorage-pd/virtuozzo-provisioner.go
+++ vzstorage-pd/virtuozzo-provisioner.go
@@ -132,15 +132,9 @@ func createPloop(mount string, options map[string]string) error {
 	// ploop driver takes kilobytes, so convert it
 	volumeSize := bytes / 1024
 
-	// create ploop deltas path
-	if err := os.MkdirAll(path.Join(mount, options["deltasPath"]), 0755); err != nil {
-		return err
-	}
-
 	ploopPath := path.Join(mount, options["volumePath"], options["volumeID"])
-	deltaPath := path.Join(mount, options["deltasPath"], options["volumeID"])
 	// Create the ploop volume
-	_, err := ploop.PloopVolumeCreate(ploopPath, volumeSize, deltaPath)
+	_, err := ploop.PloopVolumeCreate(ploopPath, volumeSize, "" /*store image in ploopPath dir*/)
 	if err != nil {
 		return err
 	}
